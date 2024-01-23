77cafdd61907a6b52b19563c02e443bf0de26998
ANU PREETHAM DANGETI <anupreetham2@gmail.com>
2024-01-23 04:01:15 -0800

    linearSearch

--- Search.java
+++ Search.java
@@ -9,7 +9,14 @@ public static void main(String[] args) {
 }
 
 public static int linearSearch(int[] nums, int target) {
-    // TODO Auto-generated method stub
+for (int i=0; i<nums.length; i++){
+    if(nums[i]==target){
+        return i;
+
+    }
+}
+    
+ 
 return -1;
 }
 }
