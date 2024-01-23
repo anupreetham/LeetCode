170e6c191d67ed274b31df1f59b0590ede6d0702
ANU PREETHAM DANGETI <anupreetham2@gmail.com>
2024-01-22 16:12:25 -0800

    initail code Test

---
+++ Search.java
@@ -0,0 +1,15 @@
+package Java_pract;
+
+public class Search {
+public static void main(String[] args) {
+    int nums[]={1,2,4,6,7,9,13,11};
+    int target=9;
+    int result=linearSearch(nums, target);
+    System.out.println("Element found at Index : "+ result);
+}
+
+public static int linearSearch(int[] nums, int target) {
+    // TODO Auto-generated method stub
+return -1;
+}
+}
