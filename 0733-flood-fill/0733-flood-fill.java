7171365357ed2d42458df3883316481304f043c4
ANU PREETHAM DANGETI <anupreetham2@gmail.com>
2024-02-20 16:08:56 -0800

    Time: 0 ms (100.00%), Space: 44.9 MB (35.80%) - LeetHub

--- 0733-flood-fill/0733-flood-fill.java
+++ 0733-flood-fill/0733-flood-fill.java
@@ -3,20 +3,20 @@ class Solution {
         if(image[sr][sc]==color){
             return image;
         }
-     int old_col= image[sr][sc];
-        fill(image,sr,sc, color,old_col);
+     int old_colour= image[sr][sc];
+        fill(image,sr,sc, color,old_colour);
         return image;
     }
 
-    public void fill(int[][] image, int sr, int sc, int color,int old_col){
-        if(sr<0 || sc<0 || sr>=image.length || sc>=image[0].length|| image[sr][sc]!=old_col ){
+    public void fill(int[][] image, int sr, int sc, int color,int old_colour){
+        if(sr<0 || sc<0 || sr>=image.length || sc>=image[0].length|| image[sr][sc]!=old_colour ){
 return;
         }
         image[sr][sc]=color;
-        fill(image, sr+1, sc, color,old_col);
-        fill(image, sr-1, sc, color,old_col);
-        fill(image, sr,  sc+1, color,old_col);
-        fill(image, sr,  sc-1, color,old_col);
+        fill(image, sr+1, sc, color,old_colour);
+        fill(image, sr-1, sc, color,old_colour);
+        fill(image, sr,  sc+1, color,old_colour);
+        fill(image, sr,  sc-1, color,old_colour);
         
     }
 }
