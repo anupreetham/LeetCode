529836017c2e9d20f5c584826628bf3cf0f33fbb
ANU PREETHAM DANGETI <anupreetham2@gmail.com>
2024-02-27 03:37:28 -0800

    Time: 19 ms (16.93%), Space: 44.6 MB (38.15%) - LeetHub

--- 0242-valid-anagram/0242-valid-anagram.java
+++ 0242-valid-anagram/0242-valid-anagram.java
@@ -1,29 +1,33 @@
 class Solution {
     public boolean isAnagram(String s, String t) {
-    HashMap<Character, Integer> shp= new HashMap<>();
+   
+    if(t.length()>s.length()){
+      return false;
+    }
+     HashMap<Character, Integer> hashMap= new HashMap<>();
     for(int i =0; i<s.length(); i++){
       char c = s.charAt(i);
-      if(!shp.containsKey(c)){
-        shp.put(c,1);
+      if(!hashMap.containsKey(c)){
+        hashMap.put(c,1);
       }
       else {
-       shp.put(c,shp.get(c)+1);
+       hashMap.put(c,hashMap.get(c)+1);
       }
     }
+
      for(int i =0; i<t.length(); i++){
         char tc = t.charAt(i);
-        if(!shp.containsKey(tc)){
-          return false;
+        if(hashMap.containsKey(tc)){
+   hashMap.put(tc,hashMap.get(tc)-1);
         }
-      if(shp.containsKey(tc)){
-         shp.put(tc,shp.get(tc)-1);
-      }
-      if(shp.containsKey(tc) && shp.get(tc)==0){
-        shp.remove(tc);
+     if(hashMap.containsKey(tc) && hashMap.get(tc)==0)
+     {
+         hashMap.remove(tc);
+        
       }
 
      }
-     return shp.isEmpty();
+     return hashMap.isEmpty();
 
     }
 }
